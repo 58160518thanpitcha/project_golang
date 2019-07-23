b65df434191035a49836b0635fa849a75f7ae782
58160518thanpitcha <58160518@go.buu.ac.th>
2019-07-23 17:20:44 +0700

    สร้างตัวประมวลผลและฟังก์ชัน post get delet

--- main.go
+++ main.go
@@ -1,43 +1,63 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"strconv"
-	
+    "encoding/json"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
 )
-const input = `
-{
-	"msg": {
-		"numfirst": "175",
-		"numlast": "221"
-	}
-}
-`
 
-type Envelope struct {
-	Type string
-	Msg  interface{}
+type Numeric struct {
+    ID        string   `json:"id,omitempty"`
+    Firstnum  int   `json:"firstnum,omitempty"`
+	Lastnum   int   `json:"lastnum,omitempty"`
+	Result    int    `json:"result,omitempty"`
+    
 }
+
+var number []Numeric
+
+func GetAllNumberEndpoint(w http.ResponseWriter, req *http.Request) {//ฟังก์ชันแสดงข้อมูลทั้งหมดที่addไว้
+    params := mux.Vars(req)
+    for _, item := range number {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Numeric{})
+}
+
+func GetNumberEndpoint(w http.ResponseWriter, req *http.Request) { //ฟังก์ชันแสดงข้อมูลตาม ID ที่addไว้
+    json.NewEncoder(w).Encode(number)
+}
+
+func CreateNumberEndpoint(w http.ResponseWriter, req *http.Request) {
+    params := mux.Vars(req)
+	var numeric Numeric
+    json.NewDecoder(req.Body).Decode(&numeric) //แปลงข้อมูลjsonที่รับมาเป็น struct
+    var result int = numeric.Firstnum+numeric.Lastnum; //คำนวณผลบวก
+    numeric.Result = result; //เก็บผลบวกใส่ไว้ใน struct
+    numeric.ID = params["id"]
+	number = append(number, numeric)
+    json.NewEncoder(w).Encode(number)// แปลง struct เป็น json เพื่อแสดงผล
+}
+func DeleteNumberEndpoint(w http.ResponseWriter, req *http.Request) { //ฟังก์ชันDeleteข้อมูล
+    params := mux.Vars(req)
+    for index, item := range number {
+        if item.ID == params["id"] {
+            number = append(number[:index], number[index+1:]...)
+            break
+        }
+    }
+    json.NewEncoder(w).Encode(number)
+}
+
 func main() {
-	var env Envelope
-	if err := json.Unmarshal([]byte(input), &env); err != nil {
-		log.Fatal(err)
-	}
-	// for the love of Gopher DO NOT DO THIS
-	var num1 string = env.Msg.(map[string]interface{})["numfirst"].(string)
-	var num2 string = env.Msg.(map[string]interface{})["numlast"].(string)
-
-	a, err := strconv.Atoi(num1)
-	if err == nil {
-		fmt.Println("a: ",a)
-	}
-	b, err := strconv.Atoi(num2)
-	if err == nil {
-		fmt.Println("b: ",b)
-	}
-	var c int = a+b;
-	fmt.Println("result: a+b = ",c)
-
+    router := mux.NewRouter()
+    router.HandleFunc("/list", GetAllNumberEndpoint).Methods("GET")
+    router.HandleFunc("/list/{id}", GetNumberEndpoint).Methods("GET")
+    router.HandleFunc("/list/{id}", CreateNumberEndpoint).Methods("POST")
+    router.HandleFunc("/list/{id}", DeleteNumberEndpoint).Methods("DELETE")
+    log.Fatal(http.ListenAndServe(":12345", router))
 }
